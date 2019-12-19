6940d9f3a90c1efb29d2b20d795e807b5e9ab19b
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-19 17:00:53 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,3 +15,17 @@ class TicTacToe
     def WIN_COMBINATIONS
       WIN_COMBINATIONS = [ [0,1,2], [3,4,5]]
       
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def player_move
+    def players = ("player 1", "player 2")
+  end
+  
+  
+  
